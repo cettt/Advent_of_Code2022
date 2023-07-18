ef1359973e726f719b78593b8b53a6a87f036beb
Cetin G <ceting@gmx.at>
2023-07-18 20:30:20 +0200

    Small improvements for day05

--- day_05.R
+++ day_05.R
@@ -1,18 +1,18 @@
-data04 <- as.list(read.fwf("Input/day05.txt", nrows = 8, widths = rep(4, 9)))
+data05 <- as.list(read.fwf("Input/day05.txt", nrows = 8, widths = rep(4, 9)))
 
-crates <- lapply(data04, \(y) gsub("\\[|\\]| ", "", y))
+crates <- lapply(data05, \(y) gsub("\\[|\\]| ", "", y))
 crates <- lapply(crates, \(y) y[y != ""])
 
 mve <- strsplit(gsub("\\D+", " ", readLines("Input/day05.txt")[-(1:10)]), " ")
-mve <- t(sapply(mve, \(x) as.integer(x[-1])))
+mve <- lapply(mve, \(x) as.integer(x[-1]))
 
 rearrange <- function(crt, .f) {
   
-  for (k in seq_along(mve[,1])) {
-    crt[[mve[k, 3]]] <- c(.f(crt[[mve[k, 2]]][seq_len(mve[k,1])]), crt[[mve[k, 3]]])
-    crt[[mve[k, 2]]] <- crt[[mve[k, 2]]][-seq_len(mve[k,1])]
+  for (m in mve) {
+    crt[[m[3]]] <- c(.f(crt[[m[2]]][seq_len(m[1])]), crt[[m[3]]])
+    crt[[m[2]]] <- crt[[m[2]]][-seq_len(m[1])]
   }
-
+  
   paste(sapply(crt, \(x) x[1]), collapse = "")
 }
 
