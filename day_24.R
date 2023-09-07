0cdf31281481bdfc41171ae0ab4cf6fb6423aa30
Cetin_G <ceting@gmx.at>
2023-09-07 22:00:45 +0200

    Optimized Day24 Reduced search to paths that get closer to target.

--- day_24.R
+++ day_24.R
@@ -14,12 +14,12 @@ check_blz <- function(k) { # function that returns times at which gr[k] is occup
   
   ns <- c(
     -rw[k] + .Internal(which(data24[,cl[k]] == "^")),
-     rw[k] - .Internal(which(data24[,cl[k]] == "v"))
+    rw[k] - .Internal(which(data24[,cl[k]] == "v"))
   )
   ns <- rep.int(ns, 28L) + rep(dm[1]*(0:27), each = length(ns))
   
   ew <- c(
-     cl[k] - .Internal(which(data24[rw[k],] == ">")),
+    cl[k] - .Internal(which(data24[rw[k],] == ">")),
     -cl[k] + .Internal(which(data24[rw[k],] == "<"))
   )
   ew <- rep.int(ew, 9L) + rep(dm[2]*(0:8), each = length(ew))
@@ -29,19 +29,23 @@ check_blz <- function(k) { # function that returns times at which gr[k] is occup
 lookup <- sapply(seq_along(data24), lookup_n)
 occ    <- sapply(seq_along(data24), check_blz)
 
+
 go_path <- function(cur, time, goal) {
   
   time <- min(occ[[cur]][occ[[cur]] >= time])
-
-    while (all(cur != goal)) {
+  dist <- if (cur == 1L) sum(dm) - (rw + cl) else rw + cl - 2L
+  
+  while (all(cur != goal)) {
     time <- time + 1L
     nxt <- unique(unlist(lookup[cur]))
     cur <- nxt[sapply(occ[nxt], \(k) all(k != time))]
-    }
+    cur <- cur[order(dist[cur])[seq_len(min(length(cur), 100L))]]
+  }
   occ <<- lapply(occ, \(x) x[x > time])
   return(time + 1L)
   
 }
+
 
 #part1---------
 p1 <- go_path(1L, 1L, length(data24))
